496962b878ce6eb33bae5193a0b156b3e4502077
Lupin <lupin.nguyen@outlook.com>
2024-04-18 19:12:33 +0700

    /feat: add sandbox command

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,5 +1,6 @@
 from commands.abstract import AbstractCommand
 from telegram import Update
+from telegram.constants import ChatType
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -29,12 +30,14 @@ class BotInstance:
         self.app = Application.builder().token(self.bot_token).build()
         self.app.add_error_handler(MessageHandler(filters.TEXT, self.handle_error))
 
+        self.app.add_handler(MessageHandler(filters.TEXT, self.handle_message))
+
     def add_handler(self, instance: AbstractCommand):
         """
         Adds a command handler to the bot application.
 
         Args:
-            command (AbstractCommand): An instance of the command to be added.
+            instance (AbstractCommand): An instance of the command to be added.
 
         Returns:
             CommandHandler: The added command handler.
@@ -46,6 +49,37 @@ class BotInstance:
 
     def handle_error(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         print(f"Update {update} caused an error: {context.error}")
+
+    def handle_message(self, text: str):
+        processed: str = text.lower()
+
+        if "hello" in processed:
+            return "Hey there"
+        if "how are you" in processed:
+            return "I am good!"
+        if "i love python" in processed:
+            return "Really!"
+
+        return "I do not understand what you wrote..."
+
+    async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        message_type: ChatType = update.message.chat.type
+        message_text: str = update.message.text
+
+        print(f'User {update.message.chat.id} in {message_type}: "{message_text}"')
+
+        if message_type == ChatType.GROUP:
+            if self.name in message_text:
+                new_text: str = message_text.replace(self.name, "").strip()
+                response: str = self.handle_response(new_text)
+            else:
+                return
+        else:
+            response: str = self.handle_response(message_text)
+
+        print(f"Bot: {response}")
+
+        await update.message.reply_text(response)
 
     def start_polling(self):
         """
