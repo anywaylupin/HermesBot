4e44c60dba2f1601a06b61fec0692deec03c52dc
Lupin <lupin.nguyen@outlook.com>
2024-04-20 01:13:26 +0700

    /refactor

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from keys import BOT_TOKEN, BOT_USERNAME
 
 
 def main():
-    my_bot = create_bot(BOT_USERNAME, BOT_TOKEN, 5)
-    my_bot.start_polling()
+    chat_bot = create_bot(BOT_USERNAME, BOT_TOKEN, 5)
+    chat_bot.start_polling()
 
 
 if __name__ == "__main__":
